4981f4adb891c8662eee2016f54eaaf662002ede
Dhrumil-Kharadi <dhumil05@gmail.com>
2024-09-15 00:22:53 +0530

    Add files via upload

---
+++ library_c.c
@@ -0,0 +1,22 @@
+#include "library.h"
+
+// Function implementations
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    if (b != 0) {
+        return (float)a / b;
+    } else {
+        return 0.0f; // or handle division by zero appropriately
+    }
+}
